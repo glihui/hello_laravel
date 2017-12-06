fe289cf6591afab572e69d97cc89c86bc20765f5
guolihui <15919476778@163.com>
2017-12-06 14:52:58 +0800

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel!-----哈哈哈
+                    Hello Laravel!-----哈哈哈....
                 </div>
 
                 <div class="links">
